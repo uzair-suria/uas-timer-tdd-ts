ac6861bc6a9fcc06d09de431ad56b7b1fdd65d47
Uzair Suria <uzair.a.suria@outlook.com>
2021-01-31 22:22:14 +0500

    Update Timer.tsx

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,26 +1,42 @@
-import React, { useState } from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import { secondsToStringParser } from '../../utils/timeParser';
 import TimerButton from '../TimerButton/TimerButton';
 import './Timer.css';
 
 const Timer = () => {
 	// const [minutes, setMinutes] = useState<number>(25);
-	const [seconds, setSeconds] = useState<number>(1520);
+	const [seconds, setSeconds] = useState<number>(25 * 60);
 	// const [milliseconds, setMilliseconds] = useState<number>(0);
 	const [isOn, setIsOn] = useState<boolean>(false);
+	// const myInterval = useRef<null | number | NodeJS.Timeout|undefined>(null);
+	const myInterval = useRef<any>(null);
+
+	useLayoutEffect(() => {
+		if (isOn === true && seconds > 0) {
+			setTimeout(() => {
+				setSeconds(seconds - 1);
+			}, 1000);
+		}
+		if (seconds === 0) {
+			setIsOn(false);
+		}
+	}, [seconds, setSeconds, isOn]);
 
 	const startTimer = () => {
-		console.log('Starting Timer');
-
-		return true;
+		// console.log('Starting Timer');
+		setIsOn(true);
 	};
 
 	const stopTimer = () => {
-		console.log('Stopping Timer');
+		// console.log('Stopping Timer');
+		clearInterval(myInterval.current);
+		setIsOn(false);
 	};
 
 	const resetTimer = () => {
-		console.log('Resetting Timer');
+		// console.log('Resetting Timer');
+		stopTimer();
+		setSeconds(25 * 60);
 	};
 	return (
 		<div className="timer-container">
